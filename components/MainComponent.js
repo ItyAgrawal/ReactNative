be1a5f5e1e6d226256fd545e67bf7be0177cd32d
ity <agrawal.ity.4aug@gmail.com>
2020-06-02 21:24:09 +0530

    forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -12,7 +12,7 @@ import Contactus from './ContactusComponent.js';
 import Aboutus from './AboutusComponent.js';
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
-
+import Reservation from './ReservationComponent';
 //automatically gets prop navigation
 
 
@@ -116,8 +116,26 @@ const mapDispatchToProps = dispatch => ({
           },
           headerTintColor: "#fff"  
         })
-});
 
+    });
+
+
+    const ReservationNavigator = createStackNavigator({
+    Reservation: { screen: Reservation }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name="menu" size={24}
+        iconStyle={{ color: 'white' }} 
+        onPress={ () => navigation.navigate('DrawerToggle') } />    
+    })
+  });
 
 
 
@@ -214,8 +232,21 @@ const MainNavigator = createDrawerNavigator({
         }, 
 
     },
-
-
+      Reservation:
+      { screen: ReservationNavigator,
+        navigationOptions: {
+          title: 'Reserve Table',
+          drawerLabel: 'Reserve Table',
+          drawerIcon: ({ tintColor, focused }) => (
+            <Icon
+              name='cutlery'
+              type='font-awesome'            
+              size={24}
+              iconStyle={{ color: tintColor }}
+            />
+          ),
+        }
+      }
 }, {
 drawerBackgroundColor: '#D1C4E9',
 contentComponent: CustomDrawerContentComponent
