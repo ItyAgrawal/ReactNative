a9ff7a0c4451d3e259f492a2d63ddb891228d908
ity <agrawal.ity.4aug@gmail.com>
2020-06-02 19:16:30 +0530

    activity indicator

--- components/AboutusComponent.js
+++ components/AboutusComponent.js
@@ -4,7 +4,7 @@ import { ListItem , Card} from 'react-native-elements';
 import { LEADERS } from '../shared/leaders.js';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -47,32 +47,62 @@ class Aboutus extends Component {
         <Card title="Our History">
             <Text style={{margin:10}}>
 
-            Started in 2010, Ristorante con Fusion quickly established itself as a culinary 
+           Started in 2010, Ristorante con Fusion quickly established itself as a culinary 
             icon par excellence in Hong Kong. With its unique brand of world fusion cuisine 
             that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong. 
             Featuring four of the best three-star Michelin chefs in the world, you never know what 
             will arrive on your plate the next time you visit us.
             The restaurant traces its humble beginnings to The Frying Pan,
             a successful chain started by our CEO, Mr. Peter Pan, 
-            that featured for the first time the world's best cuisines in a pan.
+            that featured for the first time the world best cuisines in a pan.
             </Text>
         </Card>
     );
     }
 
-    return (
-        <ScrollView style={{flex:1}}>
-            <History/>
-            <Card title="Corporate LeaderShip">
+    const RenderLeaders =()=>{
+
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+               
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+              
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                  <Card title="Corporate LeaderShip">
                 
-                <FlatList style={{maergin:1}}
+                <FlatList style={{margin:1}}
                     data={this.props.leaders.leaders}
                     renderItem={renderLeaderItem}
                     keyExtractor={item => item.id.toString()}
                 />
+                 </Card>
+            );
+        }
+    }
+
+    return (
+        <ScrollView style={{flex:1}}>
+            <History/>
             
-                
-            </Card>
+            <RenderLeaders/>
         </ScrollView>
     );
 }
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -4,6 +4,7 @@ import { ListItem ,Tile} from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -14,15 +15,9 @@ const mapStateToProps = state => {
 
 class Menu extends Component {
 
-     constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES
-        };
-    }
-
    static navigationOptions = {
         title: 'Menu'
+
     };
 
 
@@ -46,15 +41,32 @@ class Menu extends Component {
         );
     };
 
-    return (
-            <FlatList 
-                data={this.props.dishes.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}
-                />
-    );
-}
-}
+
+        if (this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return(
+                <View>            
+                    <Text>{props.dishes.errMess}</Text>
+                </View>            
+            );
+        }
+        else {
+            return (
+                <FlatList 
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}/>
+                 
+            );
+        }
+    }
+    }
+
+    
 
 //here in flastlist data should be an array
 //renderItem contains list item which have access to individual item of array and keyextractor id which is 
